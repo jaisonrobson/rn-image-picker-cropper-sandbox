cd45696474cedb755e92694178b7a861ad9659e3
jaisonrobson <xd_jrg@hotmail.com>
2022-03-16 17:48:01 -0300

    feat: envolvido nivel mais alto da arvore do sistema com o Provider do ActionSheet

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 
 import Main from './app/Main'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Main />
+    <ActionSheetProvider>
+      <View style={styles.container}>
+        <Main />
 
-      <StatusBar style="auto" />
-    </View>
+        <StatusBar style="auto" />
+      </View>
+    </ActionSheetProvider>
   );
 }
 
